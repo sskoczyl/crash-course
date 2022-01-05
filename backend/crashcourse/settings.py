5b88da4751f3e8f3fc544f0ffc8774daa26d1551
sskoczyl <szymon.skoczylas@stxnext.pl>
2022-01-05 17:39:53 +0100

    CC-592-docker-setup review fixup- last I hope

--- backend/crashcourse/settings.py
+++ backend/crashcourse/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = bool(os.environ.get("DEBUG", default=False))
+# To enable debugging enviromental variable should be set to True or 1
+DEBUG = os.environ.get("DEBUG", default=False).lower() in ('true', '1')
 
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="localhost 0.0.0.0").split(" ")
@@ -77,9 +78,9 @@ WSGI_APPLICATION = 'crashcourse.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "django_dev"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "NAME": os.environ.get("POSTGRES_DB", BASE_DIR / "django_dev"),
+        "USER": os.environ.get("POSTGRES_USER", "user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD=hello_django", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
