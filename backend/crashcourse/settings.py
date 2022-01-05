5aa8132e2399354ad007d9fcb096a51216f833b3
sskoczyl <szymon.skoczylas@stxnext.pl>
2022-01-05 15:44:12 +0100

    CC-592-docker-setup review fixup

--- backend/crashcourse/settings.py
+++ backend/crashcourse/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = bool(os.environ.get("DEBUG", default=False))
 
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="localhost 0.0.0.0").split(" ")
