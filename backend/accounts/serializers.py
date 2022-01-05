4e0e82a841096cb898d276e240ee8ff100035b25
sskoczyl <szymon.skoczylas@stxnext.pl>
2022-01-05 14:45:18 +0100

    CC-593-registration WIP

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [
+            "email",
+            "password",
+            "display_name",
+        ]
+
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def save(self):
+        account = CustomUser(
+            email = self.validated_data['email'],
+            display_name = self.validated_data['display_name'], 
+        )
+        password = self.validated_data['password']
+        
+        # VALIDATOR FOR PASSWORD ANND MAIL!
+
+        account.set_password(password)
+        account.save()
+
+        return account
