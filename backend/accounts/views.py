784bb2a0bc69205ab5fd686b098b08a537848dd7
sskoczyl <szymon.skoczylas@stxnext.pl>
2022-01-13 10:10:49 +0100

    CC-594-activate-users-account WIP

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,7 +2,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import CreateModelMixin
 
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
 from .serializers import RegistrationSerializer
+from .tokens import account_activation_token
+from .models import CustomUser
 
 
 class UserRegister(CreateModelMixin, GenericViewSet):
