fff3ed99937a6c6e9decad1966df1b0ae98feb8a
sskoczyl <szymon.skoczylas@stxnext.pl>
2022-01-13 10:10:49 +0100

    CC-594-activate-users-account WIP

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -5,7 +5,12 @@ from rest_framework.mixins import CreateModelMixin
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
 from .serializers import RegistrationSerializer
+
 from .models import ActivationToken
 
 
